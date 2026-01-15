1cfa01cb3724bdd7d3457d0bb552be3c7b94d9b6
duhasabansahan-dev <duhasabansahan@gmail.com>
2026-01-15 21:43:53 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB bağlandı"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("MK PREMIUM MOTORS BACKEND ÇALIŞIYOR");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server çalışıyor");
+});
