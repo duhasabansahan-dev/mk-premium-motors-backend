35ef57a13a7221471d060a53b14afee68ef9e3e5
duhasabansahan-dev <duhasabansahan@gmail.com>
2026-01-16 10:20:48 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-
-dotenv.config();
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
+
+// MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB bağlandı"))
-  .catch((err) => console.log(err));
-
+// TEST ROUTE
 app.get("/", (req, res) => {
   res.send("MK PREMIUM MOTORS BACKEND ÇALIŞIYOR");
 });
+
+// CARS API
 app.use("/api/cars", require("./routes/cars"));
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server çalışıyor");
+
+// DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
+
+// SERVER
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
